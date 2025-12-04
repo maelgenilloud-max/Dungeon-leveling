85eba66def6ba3a4b7bf85d480943214584e5463
Mael Genilloud <mael.genilloud@gmail.com>
2025-12-04 14:40:56 +0100

    Initial commit du projet Unity

---
+++ Assets/Script/playercontroller.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables related to player character movement
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+
+    // Health variables
+    public int maxHealth = 5;
+    int currentHealth;
+
+    Vector2 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth (int amount)
+   {
+    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    Debug.Log(currentHealth + "/" + maxHealth);
+
+   }
+}  
