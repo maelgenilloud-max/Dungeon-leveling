76017e1de1ecb2181d5869dcdc3e3c06d73038dc
Mael Genilloud <mael.genilloud@gmail.com>
2025-12-11 14:16:04 +0100

    Test après une semaine

--- Assets/Script/playercontroller.cs
+++ Assets/Script/playercontroller.cs
@@ -5,40 +5,44 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variables related to player character movement
-    public InputAction MoveAction;
-    Rigidbody2D rigidbody2d;
+  // Variables related to player character movement
+  public InputAction MoveAction;
+  Rigidbody2D rigidbody2d;
+  Vector2 move;
+  public float speed = 3.0f;
 
-    // Health variables
-    public int maxHealth = 5;
-    int currentHealth;
 
-    Vector2 move;
+  // Variables related to the health system
+  public int maxHealth = 5;
+  int currentHealth;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        MoveAction.Enable();
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+     MoveAction.Enable();
+     rigidbody2d = GetComponent<Rigidbody2D>();
+     currentHealth = maxHealth;
+  }
+ 
+  // Update is called once per frame
+  void Update()
+  {
+     move = MoveAction.ReadValue<Vector2>();
+     Debug.Log(move);
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-    }
 
-    void FixedUpdate()
-    {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
-        rigidbody2d.MovePosition(position);
-    }
-    void ChangeHealth (int amount)
-   {
-    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-    Debug.Log(currentHealth + "/" + maxHealth);
+  // FixedUpdate has the same call rate as the physics system 
+ void FixedUpdate()
+  {
+     Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+     rigidbody2d.MovePosition(position);
+  }
 
-   }
-}  
+
+  void ChangeHealth (int amount)
+  {
+     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+     Debug.Log(currentHealth + "/" + maxHealth);
+  }
+}
