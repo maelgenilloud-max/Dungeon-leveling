d9f86401bb3f3359eb9b89f491571ffc3b3b2299
Mael Genilloud <mael.genilloud@gmail.com>
2025-12-11 15:23:25 +0100

    caca

--- Assets/Script/playercontroller.cs
+++ Assets/Script/playercontroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
   // Variables related to player character movement
@@ -14,35 +15,37 @@ public class PlayerController : MonoBehaviour
 
   // Variables related to the health system
   public int maxHealth = 5;
-  int currentHealth;
+  int currentHealth = 1;
+
 
   // Start is called before the first frame update
   void Start()
   {
      MoveAction.Enable();
      rigidbody2d = GetComponent<Rigidbody2D>();
-     currentHealth = maxHealth;
+     //currentHealth = maxHealth;
   }
  
   // Update is called once per frame
   void Update()
   {
      move = MoveAction.ReadValue<Vector2>();
-     Debug.Log(move);
   }
 
 
-  // FixedUpdate has the same call rate as the physics system 
- void FixedUpdate()
+  // FixedUpdate has the same call rate as the physics system
+  void FixedUpdate()
   {
      Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
      rigidbody2d.MovePosition(position);
   }
 
 
-  void ChangeHealth (int amount)
+  public void ChangeHealth (int amount)
   {
      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
      Debug.Log(currentHealth + "/" + maxHealth);
   }
+
+
 }
